d03c5ebeb55e4b74fa3a360cc9650bd6b4aff85b
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2019-12-10 17:23:32 +0900

    自動更新機能　実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,9 @@
 $(function(){
   var reloadMessages = function() {
-    // console.log(3);
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージid取得
     last_message_id = $(".message:last").data("message-id");
-    console.log(last_message_id);
     var group_id = $(".chat-main:last").data("group_id");
     url = `../../groups/` + group_id.toString() + `/api/messages`;
-    // console.log(9);
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: url,
@@ -17,28 +14,22 @@ $(function(){
       data:{id: last_message_id}
     })
     .done(function(messages) {
-      // console.log(20);
       //追加するHTMLの入れ物を作る
       var insertHTML = '';
       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
-        // console.log(26);
       });
-      // console.log(28);
       //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML);
       // メッセージ取得後に画面スクロール
-      // console.log(32);
       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
     })
     .fail(function() {
-      // console.log(36);
       console.log('error');
     });
   };
   var buildHTML = function(message) {
-    // console.log(41);
     if (message.content && message.image) {
       //data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
@@ -90,16 +81,13 @@ $(function(){
         `</div>` +
       `</div>`
     };
-    // console.log(93);
     return html;
   };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    // console.log(98);
     var rlm = reloadMessages
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    // console.log(102);
     $.ajax({
       url: url,
       type: 'POST',
@@ -109,22 +97,18 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      // console.log(112);
       var html = buildHTML(message);
       $('.messages').append(html);
       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
       $('#new_message')[0].reset();
-      // console.log(117);
     })
     .fail(function(){
-      // console.log(120);
       alert("メッセージ送信に失敗しました");
     });
   })
-  // console.log(124);
   var group_id = $(".chat-main:last").data("group_id");
-  url = `/groups/` + group_id.toString() + `/messagese`;
+  url = `/groups/` + group_id.toString() + `/messages`;
   if(location.pathname === url) {
-    console.log(location.pathname); /*一致しなければ何もしない*/ 
+    setInterval(reloadMessages, 7000); /*一致しなければ何もしない*/
   }
 });
